44b041629a42c1c9c30fb4959247a9f2fe4cefef
brian moore <datastx@gmail.com>
2023-02-03 14:37:50 -0500

    bm/add-nav-bar

--- app/main.py
+++ app/main.py
@@ -32,87 +32,109 @@ TIME_RAN = 'Time Ran'
 ENGINE = 'Engine'
 ALL_SEARCH_HISTORY_COLUMNS =[WHAT, WHERE, FILE_NAME, TIME_RAN, ENGINE]
 
+
+MONEY_GUN = "Money Gun"
+ZOOM_INFO = "Zoom Info"
+HUB_SPOT = "Hub Spot"
+
+ALL_NAVIATION_OPTIONS = [MONEY_GUN,ZOOM_INFO, HUB_SPOT]
+
 # primer values
 if "mdf" not in st.session_state:
     st.session_state.mdf = pd.DataFrame(columns=ALL_SEARCH_HISTORY_COLUMNS)
 if "file_name" not in st.session_state:
     st.session_state.file_name = None
 
+navigation_radio_button  = st.sidebar.radio("Naviagtion",ALL_NAVIATION_OPTIONS)
 
-
-st.markdown("<h1 style='text-align: center; color: red;'>Money Gun</h1>", unsafe_allow_html=True)
-st.markdown("<h6 style='text-align: center; color: grey;'>version 1.0</h1>", unsafe_allow_html=True)
-
-
-
-engine_option = st.selectbox(
-    'Select Scraper Engine',
-    ['Indeed'])
-
-
-def add_csv_ext(str_obj: str) -> str:
-    return f"{str_obj}.csv"
-
-
-col0, col1= st.columns(2)
-what= col0.text_input(WHAT)
-where = col1.text_input(WHERE)
-
-
-but1, but2 = st.columns([1,1])
-with but1:
-    find_jobs = st.button('Find Jobs')
-with but2:
-    proxy_logs = 'https://scrapeops.io/app/dashboard'
-    if st.button('Proxy Logs'):
-        webbrowser.open_new_tab(proxy_logs)
+if navigation_radio_button == MONEY_GUN:
 
 
 
-# Critical path for running scrapy    
-if find_jobs:
-    st.session_state.file_name = add_csv_ext(new_uuid())
-    print(f"file name is {st.session_state.file_name}")
-    df_new = pd.DataFrame({WHAT: what, 
-                            WHERE: where, 
-                            FILE_NAME: st.session_state.file_name,
-                            TIME_RAN: datetime.now() ,
-                            ENGINE: engine_option}
-                            , index=[0])    
-    st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
-    what = what.strip()
-    where = where.strip()
-    # TODO: hack to share streamlit input with scrapy
-    os.environ[WHAT.upper()] = what
-    os.environ[WHERE.upper()] = where
-    call_scrappy = lambda x : f"scrapy crawl indeed_jobs -o {x}"
-    subprocess.run(call_scrappy(st.session_state.file_name).split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
-        
+
+    st.markdown("<h1 style='text-align: center; color: red;'>Money Gun</h1>", unsafe_allow_html=True)
+    st.markdown("<h6 style='text-align: center; color: grey;'>version 1.0</h1>", unsafe_allow_html=True)
 
 
-@st.cache
-def convert_df(df: pd.DataFrame) -> pd.DataFrame:
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+
+    engine_option = st.selectbox(
+        'Select Scraper Engine',
+        ['Indeed'])
 
 
-with st.expander(f"See History: count={st.session_state.mdf.shape[0]}"):
-    st.dataframe(st.session_state.mdf)
+    def add_csv_ext(str_obj: str) -> str:
+        return f"{str_obj}.csv"
 
-if st.session_state.file_name is not None:
-    with st.container():
-        st.title('Search Results')
-        try:
-            data = pd.read_csv(st.session_state.file_name,dtype=object)
-            st.dataframe(data=data)
 
-            csv = convert_df(data)
+    col0, col1= st.columns(2)
+    what= col0.text_input(WHAT)
+    where = col1.text_input(WHERE)
 
-            st.download_button(
-                label="Download data as CSV",
-                data=csv,
-                file_name=st.session_state.file_name,
-                mime='text/csv',
-            )
-        except pd.errors.EmptyDataError:
-            st.error(f'We did not get any results from indeed for your query. Check indeeds site to make sure Money Gun is working. {INDEED_URL}', icon="🚨")
+
+    but1, but2 = st.columns([1,1])
+    with but1:
+        find_jobs = st.button('Find Jobs')
+    with but2:
+        proxy_logs = 'https://scrapeops.io/app/dashboard'
+        if st.button('Proxy Logs'):
+            webbrowser.open_new_tab(proxy_logs)
+
+
+
+    # Critical path for running scrapy    
+    if find_jobs:
+        st.session_state.file_name = add_csv_ext(new_uuid())
+        print(f"file name is {st.session_state.file_name}")
+        df_new = pd.DataFrame({WHAT: what, 
+                                WHERE: where, 
+                                FILE_NAME: st.session_state.file_name,
+                                TIME_RAN: datetime.now() ,
+                                ENGINE: engine_option}
+                                , index=[0])    
+        st.session_state.mdf = pd.concat([st.session_state.mdf, df_new], axis=0)
+        what = what.strip()
+        where = where.strip()
+        # TODO: hack to share streamlit input with scrapy
+        os.environ[WHAT.upper()] = what
+        os.environ[WHERE.upper()] = where
+        call_scrappy = lambda x : f"scrapy crawl indeed_jobs -o {x}"
+        subprocess.run(call_scrappy(st.session_state.file_name).split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+            
+
+
+    @st.cache
+    def convert_df(df: pd.DataFrame) -> pd.DataFrame:
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+
+    with st.expander(f"See History: count={st.session_state.mdf.shape[0]}"):
+        st.dataframe(st.session_state.mdf)
+
+    if st.session_state.file_name is not None:
+        with st.container():
+            st.title('Search Results')
+            try:
+                data = pd.read_csv(st.session_state.file_name,dtype=object)
+                st.dataframe(data=data)
+
+                csv = convert_df(data)
+
+                st.download_button(
+                    label="Download data as CSV",
+                    data=csv,
+                    file_name=st.session_state.file_name,
+                    mime='text/csv',
+                )
+            except pd.errors.EmptyDataError:
+                st.error(f'We did not get any results from indeed for your query. Check indeeds site to make sure Money Gun is working. {INDEED_URL}', icon="🚨")
+
+elif navigation_radio_button== ZOOM_INFO:
+    st.info("This page is not built yet")
+
+
+elif navigation_radio_button == HUB_SPOT:
+    st.info("This page is not built yet")
+
+else:
+    st.info('This is the abyss. How did you get here?')
